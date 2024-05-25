9dc4ed8affd441c845b6c88b584e924ff32d44ed
Dhorminick <etiketochukwu@gmail.com>
2024-05-25 23:05:00 +0100

    nil

--- layout/alert.php
+++ layout/alert.php
@@ -2,7 +2,7 @@
 					<!-- <div class="row"> -->
 						<div class="alert-div" style="display: flex;justify-content:center;">
 						<?php foreach ($message as $error) : ?>
-							<div class="alert alert-primary alert-dismissible show col-lg-10 col-12">
+							<div class="alert alert-primary alert-dismissible show col-lg-12 col-12">
 								<div class="alert-body">
 									<button class="close close-alert" data-dismiss="alert"><span>×</span></button>
 									<?php echo $error ?>
